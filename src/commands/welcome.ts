cc01e829a4dbfe30b70bcf46b83070fa2953e83f
Shmuel Bisberg <shmuelbisberg.acc@gmail.com>
2025-06-29 22:08:24 +0300

    feat: add welcome command and multilingual welcome messages
    Signed-off-by: Shmuel Bisberg <shmuelbisberg.acc@gmail.com>

---
+++ src/commands/welcome.ts
@@ -0,0 +1,46 @@
+import i18next from "../lib/i18n";
+import { sock } from "../lib/connection";
+import { CommandConfig, CommandContext, CommandHandler } from "../types";
+import { CommandBase, DEFAULT_COMMAND_CONFIG } from "./command-base";
+import { logger } from "../lib/logger";
+
+export class Welcome extends CommandBase {
+  static config: CommandConfig = {
+    ...DEFAULT_COMMAND_CONFIG,
+    name: "welcome",
+  } as const;
+
+  constructor(args: Pick<CommandContext, "message" | "args">) {
+    super(args);
+  }
+
+  handle: CommandHandler = async () => {
+    const {
+      key: { remoteJid },
+      pushName,
+    } = this.context.message;
+
+    if (!remoteJid || !pushName || !this.context.args[0]) {
+      logger.error(
+        this.context,
+        "Cannot send welcome message: remoteJid, pushName, or phone argument missing"
+      );
+      return;
+    }
+
+    const phoneNumber = this.context.args[0].replace(/[^0-9]/g, "");
+
+    // Assert phoneNumber is a valid phone number (at least 8 digits)
+    if (phoneNumber.match(/^\d{8,}$/) === null) {
+      logger.error(this.context, "Invalid phone number provided");
+      return;
+    }
+
+    await sock.sendMessage(remoteJid, {
+      text: i18next.t("welcome", { phone: `@${phoneNumber}` }),
+      mentions: [`${phoneNumber}@s.whatsapp.net`],
+    });
+
+    logger.debug(this.context, "Sent welcome message to", remoteJid);
+  };
+}
