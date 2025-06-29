558fd79749506e9e371c3c52b05312c4bc71df02
Shmuel Bisberg <shmuelbisberg.acc@gmail.com>
2025-06-29 22:08:44 +0300

    commands: ping: Implement `ping` command for health check
    Signed-off-by: Shmuel Bisberg <shmuelbisberg.acc@gmail.com>

---
+++ src/commands/ping.ts
@@ -0,0 +1,29 @@
+import { sock } from "../lib/connection";
+import { CommandConfig, CommandHandler } from "../types";
+import { CommandBase, DEFAULT_COMMAND_CONFIG } from "./command-base";
+import { logger } from "../lib/logger";
+
+export class Ping extends CommandBase {
+  static config: CommandConfig = {
+    ...DEFAULT_COMMAND_CONFIG,
+    name: "ping",
+    description: "Checks if the bot is online by sending a 'pong' response.",
+    examples: ["ping", "p"],
+    aliases: ["p"],
+  } as const;
+
+  handle: CommandHandler = async () => {
+    const { remoteJid } = this.context.message.key;
+
+    if (!remoteJid) {
+      logger.error(this.context, "Cannot send ping response: remoteJid missing");
+      return;
+    }
+
+    await sock.sendMessage(remoteJid, {
+      text: "pong!",
+    });
+
+    logger.debug(this.context, "Sent ping response to", remoteJid);
+  };
+}
