06ddeeae91c9a551ad3b8a69139072849d722804
Shmuel Bisberg <shmuelbisberg.acc@gmail.com>
2025-06-29 22:07:33 +0300

    program: Implement graceful shutdown and event handlers for message, contact, and group updates
    Signed-off-by: Shmuel Bisberg <shmuelbisberg.acc@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-import qrcode from "qrcode-terminal";
+import "dotenv/config";
+import { startSock } from "./lib/connection";
+import { handleContactUpsert, handleGroupsUpsert, handleMessagesUpsert } from "./lib/handlers";
+import { handlePrismaDisconnect } from "./lib/prisma";
 
-qrcode.generate("https://example.com");
+const gracefulShutdown = async () => {
+  await handlePrismaDisconnect();
+  process.exit(0);
+};
+
+process.on("SIGINT", gracefulShutdown);
+process.on("SIGTERM", gracefulShutdown);
+
+startSock((sock) => {
+  // On new messages
+  sock.ev.on("messages.upsert", handleMessagesUpsert);
+
+  // On contact updates (e.g., new contacts added or existing contacts updated)
+  sock.ev.on("contacts.upsert", handleContactUpsert);
+
+  // When groups are added or updated
+  sock.ev.on("groups.upsert", handleGroupsUpsert);
+});
